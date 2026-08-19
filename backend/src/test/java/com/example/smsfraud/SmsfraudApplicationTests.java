9487bfb858a81670e20805cfbffa2f53119d5c28
Khalil357 <khalilally58@gmail.com>
2026-08-19 15:35:18 +0300

    fix: folder structure added

---
+++ backend/src/test/java/com/example/smsfraud/SmsfraudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smsfraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsfraudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
